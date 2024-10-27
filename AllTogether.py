faf328020bf5e4e7d1000f6cb2bc9575a847913a
Herm3la <ha528@kent.ac.uk>
2024-10-27 22:22:07 +0000

    P4

--- AllTogether.py
+++ AllTogether.py
@@ -1,1 +1,19 @@
 ### Part Four -- your code goes here. 
+import random
+list =[]
+
+for i in range(10):
+    i = random.randint(1,101)
+    
+    list.append(i)
+
+print("Old list:",list)
+
+pointer = 0
+while pointer < len(list):
+    if list[pointer] % 2 == 0: 
+        list.pop(pointer)       
+    else:
+        pointer += 1 
+print("New list:", list) 
+
